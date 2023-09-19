a16b7604d109b4853e7991b1d4adc180365ee8ad
faisalrehman <faisalfasi18@gmail.com>
2023-09-19 17:13:13 +0200

    on cart image displaying issue is fixed

--- client/src/components/Cart/Cart.jsx
+++ client/src/components/Cart/Cart.jsx
@@ -1,7 +1,6 @@
 import { loadStripe } from "@stripe/stripe-js";
 import makeRequest from "../../makeRequest";
-import React, { useEffect } from "react";
-import { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import "./Cart.scss";
 import DeleteOutlineOutlinedIcon from "@mui/icons-material/DeleteOutlineOutlined";
@@ -11,6 +10,7 @@ const Cart = () => {
   const products = useSelector((state) => state.cart.products);
   const dispatch = useDispatch();
   const [orderHasItems, setOrderHasItems] = useState(false);
+  const [paymentError, setPaymentError] = useState(null);
 
   const totalPrice = () => {
     let total = 0;
@@ -23,43 +23,38 @@ const Cart = () => {
   const stripePromise = loadStripe(
     "pk_test_51NgTlNLMYb3slf8ZwSq1g65zBPhFQNxRBiuufqgTcJ4f6fZOzCNslLnqhiaPPFuzzO11robVAFPg9W2rRRybdOrG00sloUrLGu"
   );
+
   useEffect(() => {
-    if (products.length > 0) {
-      setOrderHasItems(true);
-      console.log(products.length);
-    } else {
-      setOrderHasItems(false);
-      console.log(products.length);
-    }
+    setOrderHasItems(products.length > 0);
   }, [products]);
 
   const handlePayment = async () => {
-    if (orderHasItems) {
-      try {
-        const stripe = await stripePromise;
+    if (!orderHasItems) {
+      return;
+    }
 
-        const res = await makeRequest.post("/orders", {
-          products,
-        });
-        console.log(res);
-        await stripe.redirectToCheckout({
-          sessionId: res.data.stripeSession.id,
-        });
-      } catch (error) {
-        console.log(error);
-      }
+    try {
+      const stripe = await stripePromise;
+
+      const res = await makeRequest.post("/orders", {
+        products,
+      });
+      console.log(res);
+
+      await stripe.redirectToCheckout({
+        sessionId: res.data.stripeSession.id,
+      });
+    } catch (error) {
+      console.error(error);
+      setPaymentError("An error occurred during payment. Please try again.");
     }
   };
-
   return (
     <div className="cart">
       <h1>Products in your cart</h1>
       {products?.map((item) => (
         <div className="item" key={item.id}>
-          <img
-            src={process.env.REACT_APP_UPLOAD_URL + item.img}
-            alt="clothe image"
-          />
+          <img src={`${item.img}`} alt="clothe image" />
           <div className="details">
             <h1>{item.title}</h1>
             <p>{item.desc?.substring(0, 100)}</p>
@@ -79,6 +74,7 @@ const Cart = () => {
         <span>${totalPrice()}</span>
       </div>
       <button onClick={handlePayment}>PROCEED TO CHECKOUT</button>
+      {paymentError && <div className="error">{paymentError}</div>}
       {!orderHasItems && <div>Please add a product to order</div>}
       <span className="reset" onClick={() => dispatch(resetCart())}>
         Reset Cart
@@ -88,3 +84,94 @@ const Cart = () => {
 };
 
 export default Cart;
+
+// import { loadStripe } from "@stripe/stripe-js";
+// import makeRequest from "../../makeRequest";
+// import React, { useEffect } from "react";
+// import { useState } from "react";
+// import { useDispatch, useSelector } from "react-redux";
+// import "./Cart.scss";
+// import DeleteOutlineOutlinedIcon from "@mui/icons-material/DeleteOutlineOutlined";
+// import { removeItem, resetCart } from "../../redux/cartReducer";
+
+// const Cart = () => {
+//   const products = useSelector((state) => state.cart.products);
+//   const dispatch = useDispatch();
+//   const [orderHasItems, setOrderHasItems] = useState(false);
+
+//   const totalPrice = () => {
+//     let total = 0;
+//     products.forEach((item) => {
+//       total += item.quantity * item.price;
+//     });
+//     return total.toFixed(2);
+//   };
+
+//   const stripePromise = loadStripe(
+//     "pk_test_51NgTlNLMYb3slf8ZwSq1g65zBPhFQNxRBiuufqgTcJ4f6fZOzCNslLnqhiaPPFuzzO11robVAFPg9W2rRRybdOrG00sloUrLGu"
+//   );
+//   useEffect(() => {
+//     if (products.length > 0) {
+//       setOrderHasItems(true);
+//       console.log(products.length);
+//     } else {
+//       setOrderHasItems(false);
+//       console.log(products.length);
+//     }
+//   }, [products]);
+
+//   const handlePayment = async () => {
+//     if (orderHasItems) {
+//       try {
+//         const stripe = await stripePromise;
+
+//         const res = await makeRequest.post("/orders", {
+//           products,
+//         });
+//         console.log(res);
+//         await stripe.redirectToCheckout({
+//           sessionId: res.data.stripeSession.id,
+//         });
+//       } catch (error) {
+//         console.log(error);
+//       }
+//     }
+//   };
+
+//   return (
+//     <div className="cart">
+//       <h1>Products in your cart</h1>
+//       {products?.map((item) => (
+//         <div className="item" key={item.id}>
+//           <img
+//             src={process.env.REACT_APP_UPLOAD_URL + item.img}
+//             alt="clothe image"
+//           />
+//           <div className="details">
+//             <h1>{item.title}</h1>
+//             <p>{item.desc?.substring(0, 100)}</p>
+//             <div className="price">
+//               {item.quantity} x ${item.price}
+//             </div>
+//           </div>
+//           <DeleteOutlineOutlinedIcon
+//             className="delete"
+//             onClick={() => dispatch(removeItem(item.id))}
+//           />
+//         </div>
+//       ))}
+
+//       <div className="total">
+//         <span>SUBTOTAL</span>
+//         <span>${totalPrice()}</span>
+//       </div>
+//       <button onClick={handlePayment}>PROCEED TO CHECKOUT</button>
+//       {!orderHasItems && <div>Please add a product to order</div>}
+//       <span className="reset" onClick={() => dispatch(resetCart())}>
+//         Reset Cart
+//       </span>
+//     </div>
+//   );
+// };
+
+// export default Cart;
--- client/src/pages/Product/Product.jsx
+++ client/src/pages/Product/Product.jsx
@@ -16,114 +16,233 @@ const Product = () => {
   const dispatch = useDispatch();
 
   const { data, loading, error } = useFetch(`/products/${id}?populate=*`);
+  const uploadUrl = process.env.REACT_APP_UPLOAD_URL;
+
   if (loading) {
-    return "Loading...";
+    return <div>Loading...</div>;
   }
 
   if (error) {
-    return `Error: ${error.message}`;
+    return <div>Error: {error.message}</div>;
   }
+
   return (
     <div className="product">
-      {loading ? (
-        "Loading!"
-      ) : (
-        <>
-          <div className="left">
-            <div className="images">
-              <img
-                src={
-                  process.env.REACT_APP_UPLOAD_URL +
-                  data?.attributes?.img?.data?.attributes?.url
-                }
-                alt=""
-                onClick={() => setSelectedImg("img")}
-              />
-              <img
-                src={
-                  process.env.REACT_APP_UPLOAD_URL +
-                  data?.attributes?.img2?.data?.attributes?.url
-                }
-                alt=""
-                onClick={() => setSelectedImg("img2")}
-              />
-            </div>
-            <div className="mainImg">
-              {data && data.attributes && data.attributes.img && (
-                <img
-                  src={
-                    process.env.REACT_APP_UPLOAD_URL +
-                    data.attributes[selectedImg].data.attributes.url
-                  }
-                  alt=""
-                />
-              )}
-            </div>
+      <div className="left">
+        <div className="images">
+          {data?.attributes?.img && (
+            <img
+              src={`${uploadUrl}${data.attributes.img.data.attributes.url}`}
+              alt=""
+              onClick={() => setSelectedImg("img")}
+            />
+          )}
+          {data?.attributes?.img2 && (
+            <img
+              src={`${uploadUrl}${data.attributes.img2.data.attributes.url}`}
+              alt=""
+              onClick={() => setSelectedImg("img2")}
+            />
+          )}
+        </div>
+        <div className="mainImg">
+          {data?.attributes?.[selectedImg] && (
+            <img
+              src={`${uploadUrl}${data.attributes[selectedImg].data.attributes.url}`}
+              alt=""
+            />
+          )}
+        </div>
+      </div>
+      <div className="right">
+        <h1>Title</h1>
+        <span className="price">${data?.attributes?.price || 0}</span>
+        <p>{data?.attributes?.desc}</p>
+        <div className="quantity">
+          <button
+            onClick={() => setQuantity((prev) => (prev >= 1 ? prev - 1 : 0))}
+          >
+            -
+          </button>
+          {quantity}
+          <button onClick={() => setQuantity((prev) => prev + 1)}>+</button>
+        </div>
+        <button
+          className="add"
+          onClick={() =>
+            dispatch(
+              addToCart({
+                id: data.id,
+                title: data.attributes.title,
+                desc: data.attributes.desc,
+                price: data.attributes.price,
+                img: `${uploadUrl}${data.attributes.img.data.attributes.url}`,
+                quantity,
+              })
+            )
+          }
+        >
+          <AddShoppingCartIcon className="cursor-pointer" /> ADD TO CART
+        </button>
+        <div className="links">
+          <div className="item">
+            <FavoriteBorderIcon /> ADD TO WISH LIST
           </div>
-          <div className="right">
-            <h1>Title</h1>
-            <span className="price">$199</span>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias
-              animi veritatis sequi porro, tenetur ex cum repellendus delectus,
-              voluptas asperiores voluptatem hic tempore nam, tempora minus
-              temporibus inventore quia ullam quidem dignissimos corrupti? Magni
-              architecto,
-            </p>
-            <div className="quantity">
-              <button
-                onClick={() =>
-                  setQuantity((prev) => (prev >= 1 ? prev - 1 : 0))
-                }
-              >
-                -
-              </button>
-              {quantity}
-              <button onClick={() => setQuantity((prev) => prev + 1)}>+</button>
-            </div>
-            <button
-              className="add"
-              onClick={() =>
-                dispatch(
-                  addToCart({
-                    id: data.id,
-                    title: data.attributes.title,
-                    desc: data.attributes.desc,
-                    price: data.attributes.price,
-                    img: data.attributes.img.data.attributes.url,
-                    quantity,
-                  })
-                )
-              }
-            >
-              <AddShoppingCartIcon className=" cursor-pointer" /> ADD TO CARD
-            </button>
-            <div className="links">
-              <div className="item">
-                <FavoriteBorderIcon /> ADD TO WISH LIST
-              </div>
-              <div className="item">
-                <BalanceIcon /> ADD TO COMPARE
-              </div>
-            </div>
-            <div className="info">
-              <span>Vender: Polo</span>
-              <span>Product type: T-Shirt</span>
-              <span>Tag: T-Shirt, Women, Top</span>
-            </div>
-            <hr />
-            <div className="info">
-              <span>DESCRIPTION</span>
-              <hr />
-              <span>ADDITIONAL INFORMATION</span>
-              <hr />
-              <span>FAQ</span>
-            </div>
+          <div className="item">
+            <BalanceIcon /> ADD TO COMPARE
           </div>
-        </>
-      )}
+        </div>
+        <div className="info">
+          <span>Vendor: Polo</span>
+          <span>Product type: T-Shirt</span>
+          <span>Tag: T-Shirt, Women, Top</span>
+        </div>
+        <hr />
+        <div className="info">
+          <span>DESCRIPTION</span>
+          <hr />
+          <span>ADDITIONAL INFORMATION</span>
+          <hr />
+          <span>FAQ</span>
+        </div>
+      </div>
     </div>
   );
 };
 
 export default Product;
+
+// import React, { useState } from "react";
+// import "./Product.scss";
+// import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
+// import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+// import BalanceIcon from "@mui/icons-material/Balance";
+// import useFetch from "../../hooks/useFetch";
+// import { useParams } from "react-router-dom";
+// import { useDispatch } from "react-redux";
+// import { addToCart } from "../../redux/cartReducer";
+
+// const Product = () => {
+//   const [selectedImg, setSelectedImg] = useState("img");
+//   const [quantity, setQuantity] = useState(1);
+
+//   const id = useParams().id;
+//   const dispatch = useDispatch();
+
+//   const { data, loading, error } = useFetch(`/products/${id}?populate=*`);
+//   if (loading) {
+//     return "Loading...";
+//   }
+//   console.log(data);
+
+//   if (error) {
+//     return `Error: ${error.message}`;
+//   }
+//   console.log(process.env.REACT_APP_UPLOAD_URL + "/api/products");
+//   const imageUrl1 = `${process.env.REACT_APP_UPLOAD_URL}${data?.attributes?.img?.data?.attributes?.url}`;
+//   const imageUrl2 = `${process.env.REACT_APP_UPLOAD_URL}${data?.attributes?.img2?.data?.attributes?.url}`;
+
+//   return (
+//     <div className="product">
+//       {loading ? (
+//         "Loading!"
+//       ) : (
+//         <>
+//           <div className="left">
+//             <div className="images">
+//               <img
+//                 src={
+//                   process.env.REACT_APP_UPLOAD_URL +
+//                   data?.attributes?.img?.data?.attributes?.url
+//                 }
+//                 alt=""
+//                 onClick={() => setSelectedImg("img")}
+//               />
+//               <img
+//                 src={
+//                   process.env.REACT_APP_UPLOAD_URL +
+//                   data?.attributes?.img2?.data?.attributes?.url
+//                 }
+//                 alt=""
+//                 onClick={() => setSelectedImg("img2")}
+//               />
+//             </div>
+//             <div className="mainImg">
+//               {data && data.attributes && data.attributes.img && (
+//                 <img
+//                   src={
+//                     process.env.REACT_APP_UPLOAD_URL +
+//                     data.attributes[selectedImg].data.attributes.url
+//                   }
+//                   alt=""
+//                 />
+//               )}
+//             </div>
+//           </div>
+//           <div className="right">
+//             <h1>Title</h1>
+//             <span className="price">$199</span>
+//             <p>
+//               Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias
+//               animi veritatis sequi porro, tenetur ex cum repellendus delectus,
+//               voluptas asperiores voluptatem hic tempore nam, tempora minus
+//               temporibus inventore quia ullam quidem dignissimos corrupti? Magni
+//               architecto,
+//             </p>
+//             <div className="quantity">
+//               <button
+//                 onClick={() =>
+//                   setQuantity((prev) => (prev >= 1 ? prev - 1 : 0))
+//                 }
+//               >
+//                 -
+//               </button>
+//               {quantity}
+//               <button onClick={() => setQuantity((prev) => prev + 1)}>+</button>
+//             </div>
+//             <button
+//               className="add"
+//               onClick={() =>
+//                 dispatch(
+//                   addToCart({
+//                     id: data.id,
+//                     title: data.attributes.title,
+//                     desc: data.attributes.desc,
+//                     price: data.attributes.price,
+//                     img: data.attributes.img.data.attributes.url,
+//                     quantity,
+//                   })
+//                 )
+//               }
+//             >
+//               <AddShoppingCartIcon className=" cursor-pointer" /> ADD TO CARD
+//             </button>
+//             <div className="links">
+//               <div className="item">
+//                 <FavoriteBorderIcon /> ADD TO WISH LIST
+//               </div>
+//               <div className="item">
+//                 <BalanceIcon /> ADD TO COMPARE
+//               </div>
+//             </div>
+//             <div className="info">
+//               <span>Vender: Polo</span>
+//               <span>Product type: T-Shirt</span>
+//               <span>Tag: T-Shirt, Women, Top</span>
+//             </div>
+//             <hr />
+//             <div className="info">
+//               <span>DESCRIPTION</span>
+//               <hr />
+//               <span>ADDITIONAL INFORMATION</span>
+//               <hr />
+//               <span>FAQ</span>
+//             </div>
+//           </div>
+//         </>
+//       )}
+//     </div>
+//   );
+// };
+
+// export default Product;
