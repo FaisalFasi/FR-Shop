7fe79d36b97723c8815b9973f5d471f2c1fc4ae5
faisalrehman <faisalfasi18@gmail.com>
2023-09-19 10:14:09 +0200

    before postgresql working front end and backend on localhost

--- client/src/components/Cart/Cart.jsx
+++ client/src/components/Cart/Cart.jsx
@@ -1,6 +1,7 @@
 import { loadStripe } from "@stripe/stripe-js";
 import makeRequest from "../../makeRequest";
-import React from "react";
+import React, { useEffect } from "react";
+import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import "./Cart.scss";
 import DeleteOutlineOutlinedIcon from "@mui/icons-material/DeleteOutlineOutlined";
@@ -9,6 +10,7 @@ import { removeItem, resetCart } from "../../redux/cartReducer";
 const Cart = () => {
   const products = useSelector((state) => state.cart.products);
   const dispatch = useDispatch();
+  const [orderHasItems, setOrderHasItems] = useState(false);
 
   const totalPrice = () => {
     let total = 0;
@@ -21,20 +23,31 @@ const Cart = () => {
   const stripePromise = loadStripe(
     "pk_test_51NgTlNLMYb3slf8ZwSq1g65zBPhFQNxRBiuufqgTcJ4f6fZOzCNslLnqhiaPPFuzzO11robVAFPg9W2rRRybdOrG00sloUrLGu"
   );
+  useEffect(() => {
+    if (products.length > 0) {
+      setOrderHasItems(true);
+      console.log(products.length);
+    } else {
+      setOrderHasItems(false);
+      console.log(products.length);
+    }
+  }, [products]);
 
   const handlePayment = async () => {
-    try {
-      const stripe = await stripePromise;
+    if (orderHasItems) {
+      try {
+        const stripe = await stripePromise;
 
-      const res = await makeRequest.post("/orders", {
-        products,
-      });
-
-      await stripe.redirectToCheckout({
-        sessionId: res.data.stripeSession.id,
-      });
-    } catch (error) {
-      console.log(error);
+        const res = await makeRequest.post("/orders", {
+          products,
+        });
+        console.log(res);
+        await stripe.redirectToCheckout({
+          sessionId: res.data.stripeSession.id,
+        });
+      } catch (error) {
+        console.log(error);
+      }
     }
   };
 
@@ -66,6 +79,7 @@ const Cart = () => {
         <span>${totalPrice()}</span>
       </div>
       <button onClick={handlePayment}>PROCEED TO CHECKOUT</button>
+      {!orderHasItems && <div>Please add a product to order</div>}
       <span className="reset" onClick={() => dispatch(resetCart())}>
         Reset Cart
       </span>
